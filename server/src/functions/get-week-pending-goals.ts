b6f3ed773fa3dbef180c81fe091c09ccb84cc25e
lucasvrcintra <lucasrcintra@hotmail.com>
2024-09-11 17:01:33 -0300

    fix toSQL param on get week pending goals

--- server/src/functions/get-week-pending-goals.ts
+++ server/src/functions/get-week-pending-goals.ts
@@ -48,8 +48,7 @@ export async function getWeekPendingGoals() {
     .leftJoin(
       goalCompletionCounts,
       eq(goalCompletionCounts.goalId, goalsCreatedUpToWeek.id)
-    )
-    .toSQL();
+    );
 
   return {
     pendingGoals,
